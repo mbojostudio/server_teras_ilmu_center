5157358975504c10a630196c0c4478639da55ed2
mbojostudio <anharaldevaro789@gmail.com>
2024-12-28 10:28:23 +0800

    server

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ const crypto = require('crypto');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 require('dotenv').config();
-// Ambil path dari environment variable dan resolve ke path absolut
-const googleCloudKeyPath = path.resolve(__dirname, process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_KEY);
 
-// Baca dan parse file JSON kunci layanan
-const googleCloudKey = JSON.parse(fs.readFileSync(googleCloudKeyPath, 'utf8'));
-
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Perhatikan penggantian karakter \n
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
+};
 // Inisialisasi Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(googleCloudKey),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fbuses-3e232-default-rtdb.firebaseio.com/", // Ganti sesuai databaseURL Firebase Anda
 });
 // Set up multer untuk menyimpan file di server
